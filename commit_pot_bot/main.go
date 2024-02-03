44d981067d19d41335b52e0cc36dfd3e9bccea28
yoochanhong <chanhongy6@naver.com>
2024-02-04 02:39:58 +0900

    test 함수 생성

--- commit_pot_bot/main.go
+++ commit_pot_bot/main.go
@@ -7,21 +7,39 @@ import (
 	cron2 "github.com/robfig/cron"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 )
 
 var cron cron2.Cron
 
+func test(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+	if strings.Contains(m.Content, "연우") && strings.Contains(m.Content, "벌금") {
+		s.ChannelMessageSend(m.ChannelID, "김연우님의 벌금은 2500원입니다.\n또한 다음부터 스트릭이 깨질 시 2500원씩 벌금이 추가됩니다.")
+	}
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "pong")
+	}
+}
+
 func main() {
 	e := echo.New()
 	cron = *cron2.New()
-	dg, err := discordgo.New("Bot ${process.env.DISCORD_BOT_TOKEN}")
+	dg, err := discordgo.New("Bot " + "MTIwMjY4MDY4MDQ5NzAyMDk0OQ.G0Qltj.hr-XDbVPOviXsPlVjRLSQUsgyfbN-6NA-kVREc")
 	if err != nil {
 		fmt.Println("[ERROR] : 디스코드 세션 생성에 실패했습니다, ", err)
 		return
 	}
+	dg.AddHandler(test)
 	defer dg.Close()
 	cron.Start()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.String(200, "김연우 스트릭좀 관리해봅시다")
+	})
 
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
 
@@ -32,13 +50,8 @@ func main() {
 	}
 
 	fmt.Println("돌아가는중...")
+	e.Logger.Fatal(e.Start(":8080"))
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
-
-	e.GET("/", func(c echo.Context) error {
-		return c.String(200, "김연우 스트릭좀 관리해봅시다")
-	})
-
-	e.Logger.Fatal(e.Start(":8080"))
 }
